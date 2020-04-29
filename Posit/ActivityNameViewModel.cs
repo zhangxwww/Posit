658dd96c5e01fd00c97b62c95e63d77a9103d46c
zhangxwww <631133544@qq.com>
2020-04-29 15:24:17 +0800

    new activity field

---
+++ Posit/ActivityNameViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posit
+{
+    class ActivityNameViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+
+        public string ActivityName
+        {
+            get { return _name; }
+            set 
+            { 
+                _name = value;
+                OnPorpertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPorpertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Posit/NewActivityFields.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpfdemo;
+
+namespace Posit
+{
+    /// <summary>
+    /// NewActivityFields.xaml 的交互逻辑
+    /// </summary>
+    public partial class NewActivityFields : UserControl
+    {
+        public NewActivityFields ()
+        {
+            InitializeComponent();
+
+            activityNameTextBox.DataContext = new ActivityNameViewModel();
+
+            futureDatePickerWidget.BlackoutDates.AddDatesInPast();
+            futureDatePickerWidget.DataContext = new PickersViewModel();
+        }
+    }
+}
